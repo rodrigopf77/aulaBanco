65727618c330da04377549f38708750037c1e064
rodri <rodri@DESKTOP-HKU6KK9>
2020-09-21 14:43:12 -0300

    Aula sobrecarga de métodos

---
+++ src/programa/Funcionario.java
@@ -0,0 +1,38 @@
+package programa;
+
+public class Funcionario extends Pessoa{
+    
+    private double salario;
+    private double totalVendas;
+    private double comissao;
+
+    @Override
+    public void criarPessoa(){
+        super.criarPessoa();
+        
+        System.out.println("Salario: ");
+        this.salario = this.sc.nextDouble();
+        
+        System.out.println("Salario: ");
+        this.salario = this.sc.nextDouble();
+        
+    }
+    
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getTotalVendas() {
+        return totalVendas;
+    }
+
+    public void setTotalVendas(double totalVendas) {
+        this.totalVendas = totalVendas;
+    }
+
+    
+}
---
+++ src/programa/Gerente.java
@@ -0,0 +1,83 @@
+package programa;
+
+public class Gerente extends Funcionario{
+    
+    private double bonus;
+    
+    public void calculaSalario(){
+        if (this.getSalario() >= 15000) {
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.4));
+            
+        }else if(this.getSalario() >= 10000){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.35));
+            
+        }else if(this.getSalario() >= 7500){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.3));
+            
+        }else if(this.getSalario() >= 5000){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.25));
+            
+        }else{
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.1));
+        }
+        
+    }
+    //Assinatura de métodos (NOME E PARÂMENTROS)
+    //SobreCarga de método
+    public void calculaSalario(double totalVendas){
+        
+        totalVendas += totalVendas * 0.1;
+        this.setSalario(this.getSalario() + totalVendas);
+        
+        if (this.getSalario() >= 15000) {
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.4));
+            
+        }else if(this.getSalario() >= 10000){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.35));
+            
+        }else if(this.getSalario() >= 7500){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.3));
+            
+        }else if(this.getSalario() >= 5000){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.25));
+            
+        }else{
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.1));
+        }
+        
+    }
+    public void calculaSalario(double totalVendas, double bonus){
+        
+        totalVendas += totalVendas * 0.1;
+        this.setSalario(this.getSalario() + totalVendas);
+        
+        if (this.getSalario() >= 15000) {
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.4));
+            
+        }else if(this.getSalario() >= 10000){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.35));
+            
+        }else if(this.getSalario() >= 7500){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.3));
+            
+        }else if(this.getSalario() >= 5000){
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.25));
+            
+        }else{
+            this.setSalario(this.getSalario() - (this.getSalario() * 0.1));
+        }
+        
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    
+    
+    
+    
+}
